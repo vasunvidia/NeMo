06df6daf682f5dcb5412c8785fe4f947506b0232
Vasudevan Rengasamy <vrengasamy@nvidia.com>
2025-01-27 22:49:20 -0800

    Initial changes to full iteration CUDA graph

--- nemo/lightning/megatron_parallel.py
+++ nemo/lightning/megatron_parallel.py
@@ -278,6 +278,7 @@ class MegatronParallel(nn.ModuleList, Generic[ModelT]):
             num_microbatches=num_microbatches,
             seq_length=seq_length,
             step_i=step_i,
+            data_step=_data_step,
         )
         _forward_context["step"] = step
         step = self.callbacks.transform_event("on_megatron_step_start", step)
@@ -468,12 +469,13 @@ class MegatronParallel(nn.ModuleList, Generic[ModelT]):
 
         @functools.wraps(forward_step)
         def wrapped_forward_step_func(dataloader_iter, model):
-            if isinstance(data_step, _ModuleStepFunction):
-                _data_step = data_step(model)
-            else:
-                _data_step = data_step
+            #if isinstance(data_step, _ModuleStepFunction):
+            #    _data_step = data_step(model)
+            #else:
+            #    _data_step = data_step
 
-            batch = _data_step(dataloader_iter)
+            #batch = _data_step(dataloader_iter)
+            batch = dataloader_iter.pop()
             step = context["step"]
 
             if isinstance(loss_reduction, _ModuleStepFunction):
@@ -1092,6 +1094,10 @@ class MegatronStep(Generic[ModelT, DataT]):
     num_microbatches: Optional[int] = None
     step_i: Optional[int] = None
     decoder_seq_length: Optional[int] = None
+    data_step: Optional[int] = None
+    train_graph: torch.cuda.CUDAGraph = None
+    train_result = None
+    val_result = None
 
     @classmethod
     def infer(
@@ -1104,6 +1110,7 @@ class MegatronStep(Generic[ModelT, DataT]):
         seq_length: Optional[int] = None,
         num_microbatches: Optional[int] = None,
         step_i: Optional[int] = None,
+        data_step = None,
     ) -> "MegatronStep[ModelT, DataT]":
         """
         Creates a MegatronStep instance, inferring missing parameters if possible.
@@ -1135,6 +1142,28 @@ class MegatronStep(Generic[ModelT, DataT]):
             seq_length=seq_length or cls.infer_seq_length(data),
             num_microbatches=num_microbatches or cls.infer_num_microbatches(data),
             step_i=step_i,
+            data_step=data_step,
+            train_graph=MegatronStep.train_graph,
+        )
+
+    def run_step(self, data_iterator, seq_length):
+        if isinstance(self.data_step, _ModuleStepFunction):
+            _data_step = self.data_step(self.model)
+        else:
+            _data_step = self.data_step
+        data_list = []
+        for _ in range(self.num_microbatches):
+             data_list.append(_data_step(data_iterator))
+        data_list = [data_list]
+        return self.forward_backward_func(
+            forward_step_func=self.forward_step_func,
+            data_iterator=data_list,
+            model=self.model,
+            num_microbatches=self.num_microbatches,
+            seq_length=seq_length,
+            micro_batch_size=self.micro_batch_size,
+            forward_only=self.forward_only,
+            decoder_seq_length=self.decoder_seq_length,
         )
 
     def __call__(self) -> List[Any]:
@@ -1164,16 +1193,35 @@ class MegatronStep(Generic[ModelT, DataT]):
         data_iterator, seq_length = self.get_data_iterator_and_seq_length()
         seq_length = seq_length or self.seq_length
 
-        return self.forward_backward_func(
-            forward_step_func=self.forward_step_func,
-            data_iterator=data_iterator,
-            model=self.model,
-            num_microbatches=self.num_microbatches,
-            seq_length=seq_length,
-            micro_batch_size=self.micro_batch_size,
-            forward_only=self.forward_only,
-            decoder_seq_length=self.decoder_seq_length,
-        )
+        iter_data_list = []
+        for _ in range(self.num_microbatches):
+            iter_data_list.append(next(data_iterator))
+        iter_data_list = iter(iter_data_list)
+
+        if self.model.training:
+            assert self.step_i is not None
+            if self.step_i < 4:
+                MegatronStep.train_result = self.run_step (iter_data_list, seq_length)
+            elif self.step_i == 4:
+                # Capture CUDAgraph
+                capture_stream = torch.cuda.Stream()
+                MegatronStep.train_graph = torch.cuda.CUDAGraph()
+                if torch.distributed.get_rank() == 0:
+                    print ('Capturing CUDA graph')
+                with torch.cuda.graph(MegatronStep.train_graph, stream=capture_stream, capture_error_mode="global"):
+                    MegatronStep.train_result = self.run_step (iter_data_list, seq_length)
+                if torch.distributed.get_rank() == 0:
+                    print ('CUDA graph capture done')
+                MegatronStep.train_graph.replay()
+            else:
+                assert MegatronStep.train_graph is not None
+                if torch.distributed.get_rank() == 0:
+                    print ('CUDA graph capture replay')
+                MegatronStep.train_graph.replay()
+            return MegatronStep.train_result
+        else:
+            MegatronStep.val_result = self.run_step (iter_data_list, seq_length)
+            return MegatronStep.val_result
 
     def to_data_iterator_list(
         self, data: Union[DataT, Iterator[DataT], List[Iterator[DataT]]]
